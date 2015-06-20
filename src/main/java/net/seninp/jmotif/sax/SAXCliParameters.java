3fb065ebc80f3dd9784bfcd5c42db73046a868dd
seninp <seninp@gmail.com>
2015-06-20 12:20:34 +0200

    standardizing CLI arguments

---
+++ src/main/java/net/seninp/jmotif/sax/SAXCliParameters.java
@@ -0,0 +1,52 @@
+package net.seninp.jmotif.sax;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.beust.jcommander.Parameter;
+
+/**
+ * Parameters accepted by the bitmap printer and their default values.
+ * 
+ * @author psenin
+ * 
+ */
+public class SAXCliParameters {
+
+  // general setup
+  //
+  @Parameter
+  public List<String> parameters = new ArrayList<String>();
+
+  // dataset
+  //
+  @Parameter(names = { "--data", "-d" }, description = "The input file name")
+  public static String IN_FILE;
+
+  // output
+  //
+  @Parameter(names = { "--out", "-o" }, description = "The output file name")
+  public static String OUT_FILE;
+
+  // discretization parameters
+  //
+  @Parameter(names = { "--window_size", "-w" }, description = "SAX sliding window size")
+  public static int SAX_WINDOW_SIZE = 30;
+
+  @Parameter(names = { "--word_size", "-p" }, description = "SAX PAA word size")
+  public static int SAX_PAA_SIZE = 4;
+
+  @Parameter(names = { "--alphabet_size", "-a" }, description = "SAX alphabet size")
+  public static int SAX_ALPHABET_SIZE = 3;
+
+  @Parameter(names = "--strategy", description = "SAX numerosity reduction strategy")
+  public static NumerosityReductionStrategy SAX_NR_STRATEGY = NumerosityReductionStrategy.EXACT;
+
+  @Parameter(names = "--threshold", description = "SAX normalization threshold")
+  public static double SAX_NORM_THRESHOLD = 0.01;
+
+  // threading parameter
+  //
+  @Parameter(names = { "--threads", "-t" }, description = "number of threads to use)")
+  public static int THREADS_NUM = 1;
+
+}
--- src/main/java/net/seninp/jmotif/sax/SAXcliConverter.java
+++ src/main/java/net/seninp/jmotif/sax/SAXcliConverter.java
@@ -9,6 +9,7 @@ import net.seninp.util.StackTrace;
 import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
+import com.beust.jcommander.JCommander;
 
 /**
  * This implements a simple CLI tool for ad-hoc SAX discretization.
@@ -46,48 +47,70 @@ public final class SAXcliConverter {
 
     try {
 
-      if (args.length < 6 || args.length > 7) {
-        System.out.println(printHelp());
-        System.exit(-1);
-      }
+      SAXCliParameters params = new SAXCliParameters();
+      JCommander jct = new JCommander(params, args);
 
-      String dataFName = args[0];
-      double[] ts = TSProcessor.readFileColumn(dataFName, 0, 0);
-
-      Integer slidingWindowSize = Integer.valueOf(args[1]);
-      Integer paaSize = Integer.valueOf(args[2]);
-      Integer alphabetSize = Integer.valueOf(args[3]);
-
-      NumerosityReductionStrategy nrStrategy = NumerosityReductionStrategy.valueOf(args[4]
-          .toUpperCase());
-
-      Double nThreshold = Double.valueOf(args[5]);
-
-      NormalAlphabet na = new NormalAlphabet();
-      SAXProcessor sp = new SAXProcessor();
-
-      Integer threadsNum = 1;
-      SAXRecords res = null;
-
-      if (args.length > 6) {
-        threadsNum = Integer.valueOf(args[6]);
-      }
-
-      if (threadsNum > 1) {
-        ParallelSAXImplementation ps = new ParallelSAXImplementation();
-        res = ps.process(ts, threadsNum, slidingWindowSize, paaSize, alphabetSize, nrStrategy,
-            nThreshold);
+      if (0 == args.length) {
+        jct.usage();
       }
       else {
-        res = sp.ts2saxViaWindow(ts, slidingWindowSize, paaSize, na.getCuts(alphabetSize),
-            nrStrategy, nThreshold);
-      }
+        // get params printed
+        //
+        StringBuffer sb = new StringBuffer(1024);
+        sb.append("SAX CLI converter v.1").append(CR);
+        sb.append("parameters:").append(CR);
 
-      Set<Integer> index = res.getIndexes();
-      for (Integer idx : index) {
-        System.out.println(idx + COMMA + String.valueOf(res.getByIndex(idx).getPayload()));
-      }
+        sb.append("  input file:                  ").append(SAXCliParameters.IN_FILE).append(CR);
+        sb.append("  output file:                 ").append(SAXCliParameters.OUT_FILE).append(CR);
+        sb.append("  SAX sliding window size:     ").append(SAXCliParameters.SAX_WINDOW_SIZE)
+            .append(CR);
+        sb.append("  SAX PAA size:                ").append(SAXCliParameters.SAX_PAA_SIZE)
+            .append(CR);
+        sb.append("  SAX alphabet size:           ").append(SAXCliParameters.SAX_ALPHABET_SIZE)
+            .append(CR);
+        sb.append("  SAX numerosity reduction:    ").append(SAXCliParameters.SAX_NR_STRATEGY)
+            .append(CR);
+        sb.append("  SAX normalization threshold: ").append(SAXCliParameters.SAX_NORM_THRESHOLD)
+            .append(CR);
 
+        sb.append("  threads to use:         ").append(SAXCliParameters.THREADS_NUM).append(CR);
+
+        String dataFName = SAXCliParameters.IN_FILE;
+        double[] ts = TSProcessor.readFileColumn(dataFName, 0, 0);
+
+        Integer slidingWindowSize = Integer.valueOf(SAXCliParameters.SAX_WINDOW_SIZE);
+        Integer paaSize = Integer.valueOf(SAXCliParameters.SAX_PAA_SIZE);
+        Integer alphabetSize = Integer.valueOf(SAXCliParameters.SAX_ALPHABET_SIZE);
+
+        NumerosityReductionStrategy nrStrategy = SAXCliParameters.SAX_NR_STRATEGY;
+
+        Double nThreshold = Double.valueOf(args[5]);
+
+        NormalAlphabet na = new NormalAlphabet();
+        SAXProcessor sp = new SAXProcessor();
+
+        Integer threadsNum = 1;
+        SAXRecords res = null;
+
+        if (args.length > 6) {
+          threadsNum = Integer.valueOf(args[6]);
+        }
+
+        if (threadsNum > 1) {
+          ParallelSAXImplementation ps = new ParallelSAXImplementation();
+          res = ps.process(ts, threadsNum, slidingWindowSize, paaSize, alphabetSize, nrStrategy,
+              nThreshold);
+        }
+        else {
+          res = sp.ts2saxViaWindow(ts, slidingWindowSize, paaSize, na.getCuts(alphabetSize),
+              nrStrategy, nThreshold);
+        }
+
+        Set<Integer> index = res.getIndexes();
+        for (Integer idx : index) {
+          System.out.println(idx + COMMA + String.valueOf(res.getByIndex(idx).getPayload()));
+        }
+      }
     }
     catch (Exception e) {
       System.err.println("error occured while parsing parameters " + Arrays.toString(args) + CR
@@ -95,37 +118,6 @@ public final class SAXcliConverter {
       System.exit(-1);
     }
 
-  }
-
-  /**
-   * Prints the CLI help.
-   * 
-   * @return formatted help string.
-   */
-  private static String printHelp() {
-    StringBuffer sb = new StringBuffer(512);
-    sb.append("Command-line SAX conversion utility, the output printed to STDOUT ")
-        .append(CR)
-        .append("Expects 6 parameters:")
-        .append(CR)
-        .append(" [1] training dataset filename")
-        .append(CR)
-        .append(" [2] sliding window size")
-        .append(CR)
-        .append(" [3] PAA size")
-        .append(CR)
-        .append(" [4] Alphabet size")
-        .append(CR)
-        .append(" [5] numerosity reduction <NONE|EXACT|MINDIST>")
-        .append(CR)
-        .append(" [6] z-Normalization threshold value")
-        .append(CR)
-        .append(" [7] OPTIONAL: number of threads to use")
-        .append(CR)
-        .append(
-            "An execution example: $java -jar \"target/jmotif-sax-0.0.1-SNAPSHOT-jar-with-dependencies.jar\" ")
-        .append("src/resources/test-data/ecg0606_1.csv 120 7 5 EXACT 0.001 2").append(CR);
-    return sb.toString();
   }
 
 }
