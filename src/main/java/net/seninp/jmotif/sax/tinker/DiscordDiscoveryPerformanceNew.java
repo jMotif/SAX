d7868620fa1276ae88b4e7c5463be9eabef042c4
Pavel Senin <seninp@gmail.com>
2015-09-23 11:23:32 +0200

    profiling

--- src/main/java/net/seninp/jmotif/sax/tinker/DiscordDiscoveryPerformanceNew.java
+++ src/main/java/net/seninp/jmotif/sax/tinker/DiscordDiscoveryPerformanceNew.java
@@ -3,15 +3,17 @@ package net.seninp.jmotif.sax.tinker;
 import java.io.Console;
 import net.seninp.jmotif.sax.NumerosityReductionStrategy;
 import net.seninp.jmotif.sax.TSProcessor;
+import net.seninp.jmotif.sax.discord.BruteForceDiscordImplementation;
 import net.seninp.jmotif.sax.discord.DiscordRecord;
 import net.seninp.jmotif.sax.discord.DiscordRecords;
 import net.seninp.jmotif.sax.discord.HOTSAXImplementation;
+import net.seninp.jmotif.sax.registry.LargeWindowAlgorithm;
 
 public class DiscordDiscoveryPerformanceNew {
 
   private static final String TEST_DATA_FNAME = "src/resources/test-data/300_signal1.txt";
 
-  private static final int WIN_SIZE = 100;
+  private static final int WIN_SIZE = 300;
   private static final int PAA_SIZE = 4;
   private static final int ALPHABET_SIZE = 4;
 
@@ -23,18 +25,30 @@ public class DiscordDiscoveryPerformanceNew {
 
   public static void main(String[] args) throws Exception {
 
+    series = TSProcessor.readFileColumn(TEST_DATA_FNAME, 0, 0);
+
     Console c = System.console();
     if (c != null) {
       c.format("\nPress ENTER to proceed.\n");
       c.readLine();
     }
 
-    series = TSProcessor.readFileColumn(TEST_DATA_FNAME, 0, 0);
-
     DiscordRecords discordsHash = HOTSAXImplementation.series2Discords(series, DISCORDS_TO_TEST,
-        WIN_SIZE, PAA_SIZE, ALPHABET_SIZE, NumerosityReductionStrategy.EXACT, NORM_THRESHOLD);
+        WIN_SIZE, PAA_SIZE, ALPHABET_SIZE, NumerosityReductionStrategy.NONE, NORM_THRESHOLD);
     for (DiscordRecord d : discordsHash) {
       System.out.println("hotsax hash discord " + d.toString());
+    }
+
+    c = System.console();
+    if (c != null) {
+      c.format("\nPress ENTER to proceed.\n");
+      c.readLine();
+    }
+
+    DiscordRecords discordsBruteForce = BruteForceDiscordImplementation
+        .series2BruteForceDiscords(series, WIN_SIZE, DISCORDS_TO_TEST, new LargeWindowAlgorithm());
+    for (DiscordRecord d : discordsBruteForce) {
+      System.out.println("brute force discord " + d.toString());
     }
 
   }
