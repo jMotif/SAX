f0b0ef73f20384bd52e39cecc0c9626867810df2
seninp <seninp@gmail.com>
2015-10-06 21:02:42 +0200

    more coverage

--- src/test/java/net/seninp/jmotif/sax/TestNormalAlphabet.java
+++ src/test/java/net/seninp/jmotif/sax/TestNormalAlphabet.java
@@ -1,33 +1,87 @@
 package net.seninp.jmotif.sax;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
+import net.seninp.jmotif.sax.alphabet.NormalAlphabet;
 
 public class TestNormalAlphabet {
 
+  private NormalAlphabet a;
+
   @Before
   public void setUp() throws Exception {
+    a = new NormalAlphabet();
   }
 
   @Test
   public void testGetMaxSize() {
-    fail("Not yet implemented");
+    assertEquals(a.getMaxSize(), Integer.valueOf(20));
   }
 
   @Test
   public void testGetCuts() {
-    fail("Not yet implemented");
+    try {
+      for (int i = 2; i < a.getMaxSize(); i++) {
+        assertTrue(a.getCuts(i).length > 0);
+      }
+    }
+    catch (SAXException e) {
+      fail("Should not throw exception");
+    }
+
+    try {
+      @SuppressWarnings("unused")
+      double[] dd = a.getCuts(1);
+      fail("Should throw an exception");
+    }
+    catch (SAXException e) {
+      assert true;
+    }
+
+    try {
+      @SuppressWarnings("unused")
+      double[] dd = a.getCuts(21);
+      fail("Should throw an exception");
+    }
+    catch (SAXException e) {
+      assert true;
+    }
+
   }
 
   @Test
   public void testGetDistanceMatrix() {
-    fail("Not yet implemented");
-  }
 
-  @Test
-  public void testNormalAlphabet() {
-    fail("Not yet implemented");
+    try {
+      for (int i = 2; i < a.getMaxSize(); i++) {
+        assertTrue(a.getDistanceMatrix(i).length > 0);
+      }
+    }
+    catch (SAXException e) {
+      fail("Should not throw exception");
+    }
+
+    try {
+      @SuppressWarnings("unused")
+      double[][] dd = a.getDistanceMatrix(1);
+      fail("Should throw an exception");
+    }
+    catch (SAXException e) {
+      assert true;
+    }
+
+    try {
+      @SuppressWarnings("unused")
+      double[][] dd = a.getDistanceMatrix(21);
+      fail("Should throw an exception");
+    }
+    catch (SAXException e) {
+      assert true;
+    }
+
   }
 
 }
