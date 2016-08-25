9895df6e729c248963ef0026a3b28d04f80cac3c
Pavel Senin <seninp@gmail.com>
2016-08-25 15:58:34 -0600

    more tests

--- src/test/java/net/seninp/jmotif/sax/TestNormalAlphabet.java
+++ src/test/java/net/seninp/jmotif/sax/TestNormalAlphabet.java
@@ -84,4 +84,82 @@ public class TestNormalAlphabet {
 
   }
 
+  @Test
+  public void testException() {
+
+    try {
+      @SuppressWarnings("unused")
+      double[] cuts1 = a.getCuts(1);
+      fail("exception is not thrown");
+    }
+    catch (SAXException e) {
+      assert true;
+    }
+
+    try {
+      @SuppressWarnings("unused")
+      double[] cuts1 = a.getCentralCuts(1);
+      fail("exception is not thrown");
+    }
+    catch (SAXException e) {
+      assert true;
+    }
+
+    try {
+      @SuppressWarnings("unused")
+      double[] cuts1 = a.getCuts(1);
+      fail("exception is not thrown");
+    }
+    catch (SAXException e) {
+      assert true;
+    }
+
+    try {
+      @SuppressWarnings("unused")
+      double[][] dist1 = a.getDistanceMatrix(1);
+      fail("exception is not thrown");
+    }
+    catch (SAXException e) {
+      assert true;
+    }
+
+  }
+
+  @Test
+  public void testVarCutsAndDistances() {
+
+    try {
+      for (int i = 2; i < 21; i++) {
+        double[] cuts;
+        cuts = a.getCuts(i);
+        assertEquals(i - 1, cuts.length);
+      }
+    }
+    catch (SAXException e) {
+      fail("exception shall not be thrown!");
+    }
+
+    try {
+      for (int i = 2; i < 21; i++) {
+        double[] cuts;
+        cuts = a.getCentralCuts(i);
+        assertEquals(i, cuts.length);
+      }
+    }
+    catch (SAXException e) {
+      fail("exception shall not be thrown!");
+    }
+
+    try {
+      for (int i = 2; i < 21; i++) {
+        double[][] dist = a.getDistanceMatrix(i);
+        assertEquals(i, (dist[0]).length);
+      }
+    }
+    catch (SAXException e) {
+      fail("exception shall not be thrown!");
+    }
+
+  }
+
 }
